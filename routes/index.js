38fbbf269055a2fe385941dc04ea30a9315da787
Ondre <ondre.williams.77@gmail.com>
2021-04-10 08:03:37 -0700

    search fix

--- routes/index.js
+++ routes/index.js
@@ -11,51 +11,60 @@ router.get('/', async function(req, res, next) {
         include: [
             {
                 model: db.Comment,
-                    include: [
-                        {
-                            model: db.Vote
-                        }
-                    ]
+                    include: [{ model: db.Vote }]
             },
             {
                 model: db.City
             }
         ]
     });
-    for(let i = 0; i < allQuestions.length; i++){
-
-        let randomViews = Math.floor( Math.random() * 35) + 10;
-
-        let votes = 0;
-        for(let j = 0; j < allQuestions[i].Comments.length; j++){
-            votes += allQuestions[i].Comments[j].Votes.length;
-        }
-
-        allQuestions[i].totalViews = randomViews;
-        allQuestions[i].totalVotes = votes;
-        allQuestions[i].totalComments = allQuestions[i].Comments.length;
-        allQuestions[i].cityName = allQuestions[i].City.name;
-        allQuestions[i].cityId = allQuestions[i].City.id;
-    }
+    createCustomData(allQuestions);
 
     res.render('index', { title: 'a/A Express Skeleton Home', user: res.locals.user, allQuestions});
 });
 
 router.post('/search', async (req, res) => {
     const {searchQuery} = req.body;
-    const questionsFiltered = await db.Question.findAll({where:{
-        content: {
-            [Op.like]: `%${searchQuery}%`
-        }
-    }});
-    console.log(questionsFiltered);
+    const questionsFiltered = await db.Question.findAll({
+        where:{
+            content: { [Op.like]: `%${searchQuery}%` }
+        },
+        include: [
+            {
+                model: db.Comment,
+                    include: [{ model: db.Vote }]
+            },
+            {
+                model: db.City
+            }
+        ]
+    });
     const allQuestions = questionsFiltered;
-    // console.log('****************************************');
-    // console.log(res.locals.filteredQuestions);
+    createCustomData(allQuestions);
+
     res.render('index', { title: 'a/A  Home', user: res.locals.user, allQuestions });
 });
 
 router.get('/about', async(req, res)=> {
     res.render('about')
 })
+
+const createCustomData = (dataObj) => {
+    for(let i = 0; i < dataObj.length; i++){
+
+        let randomViews = Math.floor( Math.random() * 35) + 10;
+
+        let votes = 0;
+        for(let j = 0; j < dataObj[i].Comments.length; j++){
+            votes += dataObj[i].Comments[j].Votes.length;
+        }
+
+        dataObj[i].totalViews = randomViews;
+        dataObj[i].totalVotes = votes;
+        dataObj[i].totalComments = dataObj[i].Comments.length;
+        dataObj[i].cityName = dataObj[i].City.name;
+        dataObj[i].cityId = dataObj[i].City.id;
+    }
+};
+
 module.exports = router;
